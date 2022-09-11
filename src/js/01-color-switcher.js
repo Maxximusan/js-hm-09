9c556596af74ab0ddbd8b98667e935c9274578f6
Maxximusan <dragonstonecave@gmail.com>
2022-09-11 15:31:54 +0300

    1я задание, проверяю кнопки

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+const refs = {
+    ourBody: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+
+};
+
+refs.btnStart.addEventListener('click', onShowColor);
+refs.btnStop.addEventListener('click', onStopColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let timerId = null;
+
+refs.btnStop.setAttribute('disabled', true);
+
+function onShowColor() {
+    refs.btnStop.setAttribute('disabled', false);
+    refs.btnStart.setAttribute('disabled', true);
+
+    timerId = setInterval(() => {
+        randomColor = getRandomHexColor();
+        refs.ourBody.style.background = randomColor;
+    }, 1000);
+};
+
+function onStopColor() {
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+
+    clearInterval(timerId);
+};
+
