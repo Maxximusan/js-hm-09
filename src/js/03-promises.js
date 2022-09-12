d7472b03da594aaa3be13882059c5d85b1ed2c91
Maxximusan <dragonstonecave@gmail.com>
2022-09-12 22:17:53 +0300

    3я (после странных событий с библиотекой)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({ useIcon: false });
+
+const refs = {
+  form: document.querySelector('.form'),
+  createBtn: document.querySelector('[type = submit]'),
+};
+
+let inputsData = {};
+let intervalId = null;
+
+refs.form.addEventListener('input', getInputsValue);
+refs.form.addEventListener('submit', makePromises);
+
+function makePromises(e) {
+  e.preventDefault();
+  let position = 0;
+  let promiseDelay = Number(inputsData.delay);
+  intervalId = setInterval(() => {
+    position += 1;
+
+    if (position > inputsData.amount) return;
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          timeout: 5000,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          timeout: 5000,
+        });
+      });
+    promiseDelay += Number(inputsData.step);
+  }, 0);
+}
+function getInputsValue(e) {
+  inputsData[e.target.name] = e.target.value;
 }
 
-
-
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
+
